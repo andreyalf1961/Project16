4ea9893f76113efbd5fa9b7f86021b42e93b6246
andreyalf1961 <bazulin.a.i@gmail.com>
2024-11-25 13:09:36 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -3,11 +3,14 @@ from typing import Annotated
 
 app = FastAPI()
 
+
 @app.get('/user/{user_id}')
-async def user_id(user_id: int=Path(ge=1, le=100, description='Enter user ID', example='25')) -> dict:
+async def user_id(user_id: int = Path(ge=1, le=100, description='Enter user ID', example='25')) -> dict:
     return {'message': f'Вы вошли как пользователь №{user_id}'}
+
 
 @app.get('/user/{username}/{age}')
 async def user_inf(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
-                    example='andr')], age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='60')]) -> dict:
+                                                 example='andr')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='60')]) -> dict:
     return {'message': f'Информация о пользователе. Имя: {username}, Вoзраст: {age}.'}
